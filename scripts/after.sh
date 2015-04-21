55eb083e102f321e80c7c6360b8a5dff84a82286
kvasenko <kvasenko@ukr.net>
2015-04-21 11:43:02 +0300

    Updated after.sh script

--- scripts/after.sh
+++ scripts/after.sh
@@ -7,12 +7,18 @@
 
 APP_PATH="/var/www"
 SKELETON_VERSION="~0.9"
+BLUZMAN_VERSION="~0.2"
 PROJECT_NAME="bluz"
 
 if [ ! -f "$APP_PATH/bluz/composer.json" ]; then
     cd $APP_PATH
 
-    composer create-project bluzphp/skeleton="$SKELETON_VERSION" ./$PROJECT_NAME --prefer-dist
+    composer create-project bluzphp/bluzman="$BLUZMAN_VERSION" ./bluzman --prefer-dist
+
+    su - vagrant -c "cd $APP_PATH/bluzman && sh bin/install.sh"
+    export PATH=$PATH:$APP_PATH/bluzman/bin
+
+    bluzman init:all --path $APP_PATH --name $PROJECT_NAME
 
     ## Copy config files for `dev` environment
     cp /vagrant/files/configs/* /var/www/$PROJECT_NAME/application/configs/dev
